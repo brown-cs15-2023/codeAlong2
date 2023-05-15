ff2c5be423be76687405b4a462f084bb2e289fad
Lenrion <90710471+Lenrion@users.noreply.github.com>
2023-05-15 17:17:33 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package workshop2stencil;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Restaurant theVilla = new Restaurant();
+        theVilla.seeMenu();
+    }
+}
